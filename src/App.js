e51cd9549029cc54d24d39e55a125757dc20402d
Yulia-Dementeva <dyulias@mail.ru>
2021-08-06 09:43:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {createGlobalStyle} from 'styled-components'
+import React from "react";
+import Countries from "./components/Countries";
+
+const GlobalStyle = createGlobalStyle`
+html {
+  font-family: 'Roboto', sans-serif;
+}
+h2 {
+  margin: 0 0 10px;
+}`
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <GlobalStyle/>
+      <Countries/>
     </div>
   );
 }
